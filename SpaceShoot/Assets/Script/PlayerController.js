da0282fb57ed1b6e5361ba2b2926285f6957fa21
tianyuzhen <1148008482@qq.com>
2016-07-09 08:57:03 +0800

    no message

---
+++ SpaceShoot/Assets/Script/PlayerController.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+// 飞机运动速度
+var speed:float = 2.0f;
+
+function Update () { 
+	
+	if(transform.position.x<-7.1){
+		transform.position.x = 7.1;
+	}else if(transform.position.x>7.1){
+		transform.position.x = -7.1;
+	}
+	// 移动物体 （x,y,z） 按下键盘上的（"A","D","←","→"）主角在x轴方向上移动 
+	transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*speed,0,0);
+}
---
+++ SpaceShoot/Assets/Script/ProjectileController.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+// 子弹的运动速度
+var speed:float = 10.0f;
+
+function Update () {
+	if(transform.position.y>5.4){
+		transform.position.y = -4;
+	} 
+	// 移动物体 （x,y,z） 按下键盘上的（"A","D","←","→"）主角在x轴方向上移动 
+	transform.Translate(0,Time.deltaTime*speed,0);
+}
